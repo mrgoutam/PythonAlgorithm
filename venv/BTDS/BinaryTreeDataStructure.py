b55587ab42f50c94c16cf31179f57715e4c80ca4
Goutam-Xtremsoft <goutam@xtremsoftindia.com>
2020-08-02 18:14:59 +0530

    Binary Tree DS started

---
+++ venv/BTDS/BinaryTreeDataStructure.py
@@ -0,0 +1,22 @@
+"""
+Resources: https://www.geeksforgeeks.org/binary-tree-data-structure/
+
+A tree whose elements have at most 2 children is called a binary tree. Since each element in a binary
+tree can have only 2 children, we typically name them the left and right child.
+
+                                      1
+                                  /      \
+                                2          3
+                             /    \      /   \
+                           4       5    6     7
+                          /      /   \          \
+                        8       9     10         11
+
+
+A Binary Tree node contains following parts.
+
+1. Data
+2. Pointer to left child
+3. Pointer to right child
+
+"""
