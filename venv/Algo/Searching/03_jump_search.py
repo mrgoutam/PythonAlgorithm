9a8fb7ea158193e8baa5a215fc23d6ea3095ddab
Goutam-Xtremsoft <goutam@xtremsoftindia.com>
2020-09-13 12:40:31 +0530

    Jump search done

---
+++ venv/Algo/Searching/03_jump_search.py
@@ -0,0 +1,31 @@
+import math
+
+def jump_search(arr, x):
+    n = len(arr)
+    step = int(math.floor(math.sqrt(n)))
+    prev = 0
+    while arr[min(step, n) - 1] < x:
+        prev = step
+        step += step
+        if prev >= n:
+            return -1
+
+    while arr[prev] < x:
+        prev += 1
+        if prev == min(step, n):
+            return -1
+        if arr[prev] == x:
+            return prev
+    return -1
+
+
+if __name__=='__main__':
+    arr_ = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 700]
+    x_ = 800
+    print(f"Number {x_} is at index {jump_search(arr_, x_)}")
+
+'''
+------------------------OUTPUT BOX----------------------------------------
+Number 800 is at index -1
+--------------------------------------------------------------------------
+'''
