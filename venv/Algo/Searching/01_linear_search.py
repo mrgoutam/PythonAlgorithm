e2cecf5c5eb6f00b8005d39ac2a096f354a03d92
Goutam-Xtremsoft <goutam@xtremsoftindia.com>
2020-09-13 09:32:42 +0530

    Linear search done

---
+++ venv/Algo/Searching/01_linear_search.py
@@ -0,0 +1,17 @@
+def linear_search(sequence, target):
+    for index, item in enumerate(sequence):
+        if item==target:
+            return index
+    return None
+
+if __name__=='__main__':
+    user_input = input('Enter numbers separated by comma:\n').strip()
+    sequence_ = [int(item) for item in user_input.split(',')]
+
+    target_ = input('Enter a single number to be found in the list: \n')
+    target_ = int(target_)
+    result = linear_search(sequence_, target_)
+    if result is not None:
+        print(f'{target_} found at position: {result}')
+    else:
+        print('Target not found')
